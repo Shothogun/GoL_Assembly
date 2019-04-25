61c66c8ee24ead559edf378fc5b57333ee2ae0bd
Otho T.Komatsu <33355906+Shothogun@users.noreply.github.com>
2019-04-24 21:07:22 -0300

    Add files via upload

---
+++ Jogo_da_vida.asm
@@ -0,0 +1,360 @@
+.data
+
+Window:		.word		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				
+# Matrix 18x18 ( Matrix's 16x16 with a 0's border)
+Matrix1:	.word		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+								
+# Matrix 18x18 ( Matrix's 16x16 with a 0's border)
+Matrix2:	.word		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+
+blue:		.word 0xFF
+green:		.word 0xFF00
+red:		.word 0xFF0000
+In1:		.string "Insira a quantidade de pontos: "
+In2:		.string "Insira a coordenada x: "
+In3:		.string "Insira a coordenada y: "
+
+.text
+
+
+	li a7, 4		# Prints request
+	la a0, In1
+	ecall
+	
+	li s11, 0
+	li a7, 5		# Read dots quantity
+	ecall
+	mv s7,a0		# s1: Dots quantity
+		
+init:
+	
+	la s3, Matrix1		# s3: Matrix1(18x18)
+	addi s3, s3, 76		# Reinit reference at (0,0) - Avoid border
+	
+	la s1, Matrix2		# s1: Matrix2(18x18)
+	addi s1, s1, 76		# Same case from previous
+	
+	li a7, 4		# Prints request
+	la a0, In2
+	ecall
+	
+	li a7, 5		# Read x
+	ecall	
+	mv s4, a0 		# s4: x
+	
+	li a7, 4		# Prints request
+	la a0, In3
+	ecall
+
+	li a7, 5		# Read y
+	ecall	
+	mv s5, a0 		# s5: y
+	
+	mv a0, s4
+	mv a1, s5
+	mv a2, s3
+	call writem
+
+	mv a0, s4
+	mv a1, s5
+	mv a2, s1
+	call writem
+
+	addi s7, s7, -1
+	bnez s7, init		# ----------- Loop ----------------- 
+	
+	mv a0, s3
+	call plotm
+
+neigh_init:
+
+	la s3, Matrix1		# s3: Matrix1(18x18)
+	addi s3, s3, 76		# Reinit reference at (0,0) - Avoid border
+	
+	la s2, Matrix2		# s2: Matrix2(18x18)
+	addi s2, s2, 76		# Same case from previous
+
+	li s4, 0		# s4: column iterator
+	li s5, 0		# s5: row iterator
+	li s1, 16		# s1: Size
+	li s7, 3		# s7: 3
+	li s6, 2		# s6: 2
+	
+# Neighbour life cicle
+
+neighbour:
+# Verifies bacteria neighbour
+	li s8, 0		# s8: Alive bacterium
+	
+	lw t5,-76(s3)		# t5: Neighbour state
+	add s8, s8, t5		# Verifies top-left bacteria
+
+	lw t5,-72(s3)		# Verifies top bacteria
+	add s8, s8, t5
+	
+	lw t5,-68(s3)		# Verifies top-right bacteria
+	add s8, s8, t5
+
+	lw t5,-4(s3)		# Verifies left bacteria
+	add s8, s8,t5
+
+	lw t5,4(s3)		# Verifies right bacteria
+	add s8, s8, t5
+	
+	lw t5,68(s3)		# Verifies bottom-left bacteria
+	add s8, s8, t5
+
+	lw t5,72(s3)		# Verifies bottom bacteria
+	add s8, s8, t5
+	
+	lw t5,76(s3)		# Verifies bottom-right bacteria
+	add s8, s8, t5
+	
+	# Default address - used as argument to readm	
+	la t0, Matrix1		# s3: Matrix1(18x18)
+	addi t0, t0, 76		# Reinit reference at (0,0) - Avoid border
+	la t1, Matrix2		# s2: Matrix2(18x18)
+	addi t1, t1, 76		# Same case from previous	
+	mv a0, s4
+	mv a1, s5
+	mv a2, t0
+	call readm
+	
+	bnez a0, occupied
+	
+empty:	
+	# -------------------------Debug-----------------------------
+#	li a7, 1
+#	li a0, 1111
+#	ecall		
+
+	bne s8, s7, next_column
+
+birth:	
+	# -------------------------Debug-----------------------------
+#	li a7, 1
+#	li a0, 2222
+#	ecall
+	
+	# Default address - used as argument to readm	
+	la t0, Matrix1		# s3: Matrix1(18x18)
+	addi t0, t0, 76		# Reinit reference at (0,0) - Avoid border	
+	la t1, Matrix2		# s2: Matrix2(18x18)
+	addi t1, t1, 76		# Same case from previous
+	mv a0, s4
+	mv a1, s5
+	mv a2, t1
+	call writem
+	
+	b next_column
+
+occupied:
+	# -------------------------Debug-----------------------------
+#	li a7, 1
+#	li a0, 3333
+#	ecall
+
+	beq s8, s6, next_column
+	beq s8, s7, next_column
+	
+death:
+	# -------------------------Debug-----------------------------
+#	li a7, 1
+#	li a0, 4444
+#	ecall
+	
+	# Default address - used as argument to readm	
+	la t0, Matrix1		# s3: Matrix1(18x18)
+	addi t0, t0, 76		# Reinit reference at (0,0) - Avoid border
+	la t1, Matrix2		# s2: Matrix2(18x18)
+	addi t1, t1, 76		# Same case from previous
+	mv a0, s4
+	mv a1, s5
+	mv a2, t1
+	call writem
+				
+next_column:
+	addi s3, s3, 4		# go to next element in current matrix
+	addi s2, s2, 4		# go to next element in future matrix
+	addi s4, s4, 1
+	
+	bne s4, s1, neighbour
+
+next_row:
+	addi s3, s3, 8		# Access first element from next row(steps 1 elements)
+	addi s2, s2, 8	
+	addi s5, s5, 1
+	li s4, 0		# s4: column iterator
+	bne s5, s1, neighbour
+	li s5, 0		# s5: row iterator
+		
+init_copy_matrix:
+
+	la s1, Matrix1	# s1: Matrix1
+	la s2, Matrix2	# s2: Matrix2
+	
+	li s3, 324			# s3: Elements number
+		
+copy_matrix:
+	lw t2, 0(s2)	# Copy value from Matrix2 to Matrix1
+	sw t2, 0(s1)
+		
+	addi s3, s3, -1
+	addi s1, s1, 4
+	addi s2, s2, 4
+	bnez s3, copy_matrix
+
+update:
+	la s1, Matrix1	# s1: Matrix1
+	addi s1, s1, 76 # Reinit reference at (0,0) - Avoid border
+
+	mv a0, s1
+	call plotm
+
+	# -------------------------Debug-----------------------------
+#	addi s11, s11, 1
+#	li a7, 1
+#	add a0, s11, zero
+#	ecall
+	
+	li s4, 0		# s4: column iterator
+	li s5, 0		# s4: column iterator
+	
+#	li a7, 10
+#	ecall			# -------------- finish -------------------------
+			
+	b neigh_init
+		
+# Input	
+# a0: Matrix's adress
+plotm:
+	li t4, 0	# t4: element at row
+	li t5, 0	# t5: row
+	li t6, 16	# t6: Last one's after element 	
+	
+	la t2, Window	# t2: Bitmap window
+	la t0, blue	# t0: Blue's adress
+	lw t1, 0(t0)	# t1: Blue value
+		
+pixel_verify:
+	lw t3, 0(a0)	# t3: Space state
+	beqz t3, pixel_erase
+	
+pixel_filling:
+	sw t1, 0(t2)
+	b next_pixel
+	
+pixel_erase:
+	sw t3, 0(t2)
+
+next_pixel:
+	addi a0, a0, 4	# Steps matrix 
+	addi t2, t2, 4	# Steps pixel
+	addi t4, t4, 1  
+	
+	bne t4, t6, pixel_verify
+	
+next_pixel_row: 
+	li t4, 0	# t4: Column
+	addi t5, t5, 1	# t5: Row
+	addi a0, a0, 8	# Access first element from next row(steps 2 elements)
+	
+	bne t5, t6, pixel_verify 
+								
+	ret
+	
+# Input	
+#a0: column
+#a1: row
+#a2: Matrix's adress
+writem:	
+	li  t4, 72		# t4: offset constant(18 times 4 bytes) 
+	mul t5, t4, a1		# t5: Defines row
+	li  t4, 4		
+	mul t6, t4, a0		# t6: Defines column
+	li  t0, 0
+	add t0, t5, t6		# t0: Offset
+	add a2, a2, t0
+	
+	lw t2, 0(a2)		# t2: Dot value
+	
+	li t3, 1		# t3: Invert constant
+	xor t2, t3, t2		# Invert value
+	sw t2, 0(a2)
+	
+	ret
+	
+# Input
+#a0: column
+#a1: row
+#a2: Matrix's adress
+
+# Output
+#a0: value at matrix(1 or 0)
+readm:	
+	li  t4, 72		# t4: offset constant(18 times 4 bytes) 
+	mul t5, t4, a1		# t5: Defines row
+	li  t4, 4		
+	mul t6, t4, a0		# t6: Defines column
+	li  t0, 0
+	add t0, t5, t6		# t0: Offset
+	add a2, a2, t0
+
+	lw a0, 0(a2)		# a0: Space state
+			
+	ret
+	
+	
+	
+	
+	
